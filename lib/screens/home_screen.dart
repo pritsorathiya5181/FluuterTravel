e1947a9c3a51d1103705111e378e95d2b77a91b1
prit <prit.sorathiya@gmail.com>
2020-06-19 13:55:50 +0530

    "Add category icons"

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,10 +7,74 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        padding: EdgeInsets.symmetric(vertical: 30.0),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 120.0),
+            child: Text(
+              'What you would like to find?',
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // children: <Widget>[
+            //   _buildIcon(0),
+            //   _buildIcon(1),
+            //   _buildIcon(2),
+            //   _buildIcon(3),
+            // ],
+            children: _icons
+                .asMap()
+                .entries
+                .map((MapEntry e) => _buildIcon(e.key))
+                .toList(),
+          )
+        ],
+      )),
     );
   }
 }
